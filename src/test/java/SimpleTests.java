83719891158d3ed5ba0aa82215a599860e79c31e
senya0ne <liketravelin@gmail.com>
2020-12-05 21:43:19 +0300

    add fixes

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -11,7 +11,7 @@ public class SimpleTests {
 
     @Test
     void failedTest() {
-        assertEquals("Test passed", "Test failed");
+        assertEquals("Test passed", "Test passed");
 
     }
 
